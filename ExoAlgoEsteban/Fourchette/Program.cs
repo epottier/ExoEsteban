8e80ee6e0303d17142855652f762d0be39394a73
Esteban-dev <74709808+Esteban-dev@users.noreply.github.com>
2021-06-11 10:58:14 +0200

    ajout

--- ExoAlgoEsteban/Fourchette/Program.cs
+++ ExoAlgoEsteban/Fourchette/Program.cs
@@ -19,34 +19,54 @@ namespace Fourchette
             limite_inferieur = 0;
             limite_superieur = 100;
             nombre_ordinateur = new Random().Next(limite_inferieur, limite_superieur);
+
+            //saisie du nombre de l'utilisateur
+            #region saisie
             Console.WriteLine("Bonjour dans ce  jeux de la fourchette l'ordinateur à pris un nombre entier naturel situé entre 0 et 100");
             Console.WriteLine(", a vous de trouver ce nombre en cherchant entre les limites supérieurs et inférieurs.");
-            nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
-
-
-
-
-            //traitement
             do
             {
-                //répéter?
-                while (nombre_propose_par_Joueur != nombre_ordinateur)
+                nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
+                if (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur)
                 {
-                    if ( limite_inferieur < nombre_propose_par_Joueur << nombre_ordinateur )
-                    {
-                        limite_inferieur = nombre_propose_par_Joueur;
-                    }
-                    else if ( nombre_ordinateur << nombre_propose_par_Joueur < limite_superieur )
-                    {
-                        limite_superieur = nombre_propose_par_Joueur;
-                    }
-                    Console.WriteLine("Si vous n'avez pas trouver le bon nombre alors, choisissez-en un autre.");
-                    nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Le nombre n'est pas dans l'intervalle " + limite_inferieur + " et " + limite_superieur + " Veuillez me donner un nouveau nombre svp:");
                 }
-                
             }
             while (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur);
-            
+            #endregion
+
+
+            //saisie du nombre de  l'utilisateur
+            while (nombre_propose_par_Joueur!= nombre_ordinateur)
+            {
+
+                if (limite_inferieur > nombre_propose_par_Joueur)
+                {
+                    limite_inferieur = nombre_propose_par_Joueur;
+                }
+                else if (nombre_propose_par_Joueur > limite_superieur)
+                {
+                    limite_superieur = nombre_propose_par_Joueur;
+                }
+
+                //saisie du nombre de l'utilisateur
+                #region saisie d'un nombre
+                Console.WriteLine("Bonjour dans ce  jeux de la fourchette l'ordinateur à pris un nombre entier naturel situé entre "+limite_inferieur+" et "+limite_superieur);
+                Console.WriteLine(", a vous de trouver ce nombre en cherchant entre les limites supérieurs et inférieurs.");
+                do
+                {
+                    nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
+                    if (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur)
+                    {
+                        Console.WriteLine("Le nombre n'est pas dans l'intervalle " + limite_inferieur + " et " + limite_superieur + " Veuillez me donner un nouveau nombre svp:");
+                    }
+                }
+                while (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur);
+                #endregion
+
+            }
+
+            Console.WriteLine("Bravo vous avez trouvé");
         }
     }
 }
