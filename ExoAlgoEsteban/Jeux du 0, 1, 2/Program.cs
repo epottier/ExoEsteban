7f6093b1143be8e1e613999dbe098d87cd2b87fa
Esteban-dev <74709808+Esteban-dev@users.noreply.github.com>
2021-06-18 09:30:09 +0200

    ajout

--- ExoAlgoEsteban/Jeux du 0, 1, 2/Program.cs
+++ ExoAlgoEsteban/Jeux du 0, 1, 2/Program.cs
@@ -28,52 +28,47 @@ namespace Jeux_du_0__1__2
             Console.WriteLine("ainsi que les valeurs négative ou  supérieur à 2 ne seront paris en compte.");
             nombre_propose_par_Joueur = 1;
 
-            while ((parties_gagnées_par_joueur != 10 )|| (parties_gagnées_par_ordinateur != 10) || (nombre_propose_par_Joueur < 0))
+            while ((parties_gagnées_par_joueur != 10 ) && (parties_gagnées_par_ordinateur != 10) && (nombre_propose_par_Joueur >= 0))
             {
+                Console.WriteLine("Veuillez définir un nouveau nombre.");
                 nombre_ordinateur = new Random().Next (limite_inferieur, limite_superieur);
                 nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
                 int résultat_ordinateur = nombre_ordinateur - nombre_propose_par_Joueur;
                 int résultat_Joueur = nombre_propose_par_Joueur - nombre_ordinateur;
 
-                if (nombre_propose_par_Joueur < -1)
+                if (nombre_propose_par_Joueur >= 0)
                 {
-                    Console.WriteLine("Le Joueur à quitter la partie");
-                }
-                else if ((nombre_propose_par_Joueur >= 0) || (nombre_propose_par_Joueur <= 2))
-                {
-                   if (résultat_ordinateur == 0 || résultat_Joueur == 0)
+                    if ((nombre_propose_par_Joueur >= 0) || (nombre_propose_par_Joueur <= 2))
                     {
-                        Console.WriteLine("Aucun des deux ne  gagnent");
+                        if (résultat_ordinateur == 0 || résultat_Joueur == 0)
+                        {
+                            Console.WriteLine("Aucun des deux ne  gagnent");
+                        }
+                        else if (résultat_ordinateur == 1 || résultat_Joueur == 1)
+                        {
+                            if (résultat_ordinateur < résultat_Joueur)
+                            {
+                                parties_gagnées_par_ordinateur++;
+                            }
+                            else
+                            {
+                                parties_gagnées_par_joueur++;
+                            }
+                            Console.WriteLine("Les points sont de " + parties_gagnées_par_ordinateur + " pour l'ordinateur à " + parties_gagnées_par_joueur + " pour le Joueur");
+                        }
+                        else if (résultat_ordinateur == 2 || résultat_Joueur == 2)
+                        {
+                            if (résultat_ordinateur > résultat_Joueur)
+                            {
+                                parties_gagnées_par_ordinateur++;
+                            }
+                            else
+                            {
+                                parties_gagnées_par_joueur++;
+                            }
+                            Console.WriteLine("Les points sont de " + parties_gagnées_par_ordinateur + " pour l'ordinateur à " + parties_gagnées_par_joueur + " pour le Joueur");
+                        }
                     }
-                    else if (résultat_ordinateur == 1 || résultat_Joueur == 1)
-                    {
-                        if (résultat_ordinateur < résultat_Joueur)
-                        {
-                            parties_gagnées_par_ordinateur++;
-                        }
-                        else
-                        {
-                            parties_gagnées_par_joueur++;
-                        }
-                        Console.WriteLine("Les points sont de "+parties_gagnées_par_ordinateur+" pour l'ordinateur à "+parties_gagnées_par_joueur+" pour le Joueur");
-                    }
-                    else if (résultat_ordinateur == 2 || résultat_Joueur == 2)
-                    {
-                        if (résultat_ordinateur > résultat_Joueur)
-                        {
-                            parties_gagnées_par_ordinateur++;
-                        }
-                        else
-                        {
-                            parties_gagnées_par_joueur++;
-                        }
-                        Console.WriteLine("Les points sont de " + parties_gagnées_par_ordinateur + " pour l'ordinateur à " + parties_gagnées_par_joueur + " pour le Joueur");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Le Joueur doit choisir un nouveau nombre");
-
                 }
             }
 
@@ -86,6 +81,10 @@ namespace Jeux_du_0__1__2
             else if(parties_gagnées_par_ordinateur == 10)
             {
                 Console.WriteLine("L'ordinateur à gagner la partie");
+            }
+            else if (nombre_propose_par_Joueur < 0)
+            {
+                Console.WriteLine("Le Joueur à quitter la partie");
             }
         }
     }
