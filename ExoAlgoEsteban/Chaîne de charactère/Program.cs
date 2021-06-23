a9141f75e68aa485da99f5af62d271e0976c1269
Esteban-dev <74709808+Esteban-dev@users.noreply.github.com>
2021-06-23 16:03:14 +0200

    ajout

--- ExoAlgoEsteban/Chaîne de charactère/Program.cs
+++ ExoAlgoEsteban/Chaîne de charactère/Program.cs
@@ -6,11 +6,33 @@ namespace Chaîne_de_charactère
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez insérer une phrase");
+            Console.WriteLine("Veuillez insérer une phrase en mettant bien un '.'.");
             string messageLu = Console.ReadLine();
-            Console.WriteLine("Veuillez écrire un caractère");
-            string caractèreCherché = Console.ReadLine();
+            Console.WriteLine("Veuillez écrire un caractère.");
+            char caractèreCherché = char.Parse(Console.ReadLine());
+            int c = 0;
+            if (messageLu == ".")
+            {
+                Console.WriteLine("LE MESSAGE EST VIDE");
+            }
+            else
+            {
+                for( int i =0; i< messageLu.Length-1;i++ )
+                {
+                    char caractereALapositioni= messageLu[i];
+                    if (caractèreCherché == caractereALapositioni)
+                    {
+                        c++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Le caractère n'est pas présent dans le message.");
+                    }
+                }
 
+                ///afficage du resultat
+                Console.WriteLine("Il exacactement " + c + "fois la présence de ce caractère dans le message");
+            }
         }
     }
 }
